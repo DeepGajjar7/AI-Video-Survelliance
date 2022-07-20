61fa2eeb51fb247ee5897b7a24d1146cfb034dde
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-07-20 20:38:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var video="";
+var status1="";
+
+function preload(){
+    video=createVideo('video.mp4')
+    video.hide();
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center()
+}
+
+function draw(){
+    image(video,0,0,480,380)
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modeloaded);
+    document.getElementById("status").innerHTML="Status :Detecting Objects";
+}
+
+function modeloaded(){
+    console.log("modeloaded")
+    status1=true
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
