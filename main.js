9a941ef659af9f046b375cbb9859b051ca576f94
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-07-26 19:56:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var video="";
 var status1="";
+var objects=[];
 
 function preload(){
     video=createVideo('video.mp4')
@@ -13,6 +14,19 @@ function setup(){
 
 function draw(){
     image(video,0,0,480,380)
+    if(status1 !=""){
+        objectDetector.detect(video,gotresults)
+        for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="Status: Objects Detected"
+        document.getElementById("number_of_objects").innerHTML="Number of objects:"+objects.length;
+        fill("red");
+        percent=floor(objects[i].confidence*100)
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function start(){
@@ -26,4 +40,14 @@ function modeloaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotresults(error,results){
+    if (error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        objects=results;
+    }
 }
